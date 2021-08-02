4b7529538cc76e0a90f5ac2d57ce8bd447ec5845
Kim, MinJong <kmjc6611@gmail.com>
2021-08-02 23:06:29 +0900

    #3.6 MiddleWare ( app.use)

--- src/server.js
+++ src/server.js
@@ -11,6 +11,23 @@ const handleHome = (req, res) =>{
     return res.send("답장 보낸다. ㅋ");
 };
 
-app.get("/",middleWare, handleHome);
+const privateMiddleware = (req, res, next) => {
+    const url = req.url;
+    if (url === "/protected"){
+        return res.send("<h1>Not Allowed</h1>");
+    }
+    console.log("Allowed, you may continue.");
+    next();
+};
+
+const handleProtected = (req,res) => {
+    return res.send("비밀 공간인데 어떻게 들어왔지?")
+}
+
+app.use(middleWare);
+app.use(privateMiddleware);
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
+
 
 app.listen(4000, () => console.log("Server On 4000 Port"));
