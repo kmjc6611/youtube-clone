150e668242fbb5d003d27094b3be942e3eaa1533
Kim, MinJong <kmjc6611@gmail.com>
2021-08-02 22:56:49 +0900

    #3.4 middleWare

--- src/server.js
+++ src/server.js
@@ -2,10 +2,15 @@ import express from "express";
 
 const app = express();
 
+const middleWare = (req, res, next) => {
+    console.log("I'm the middleWare!")
+    next();
+}
+
 const handleHome = (req, res) =>{
     return res.send("답장 보낸다. ㅋ");
 };
 
-app.get("/",handleHome);
+app.get("/",middleWare, handleHome);
 
 app.listen(4000, () => console.log("Server On 4000 Port"));
